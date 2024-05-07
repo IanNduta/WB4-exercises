eb5c83bb8e1573903f66a9d8bd0f6043e9f31919
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-07 14:33:03 -0400

    exercise 1

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let myFirstArray = ["ian", "bob", "tom", "kevin"]
+let arraySize = myFirstArray.length;
+
+for (let i = 0; i < arraySize; i++){
+    console.log(myFirstArray[i]);
+}
+
+
+
+
+
+
